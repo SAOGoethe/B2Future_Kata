d19760187a5862953d51bc4b9e140fcd58a25121
Axel Mc Allister <axelma@ext.inditex.com>
2024-11-08 17:25:07 +0100

    Change date String to LocalDAte

--- src/main/java/org/example/Inventory.java
+++ src/main/java/org/example/Inventory.java
@@ -1,18 +1,19 @@
 package org.example;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Inventory {
-private final Map<String, ArrayList<String>> stock = new HashMap<>();
+private final Map<LocalDate, ArrayList<String>> stock = new HashMap<>();
 
-  public List<String> project(String date) {
+  public List<String> project(LocalDate date) {
     return this.stock.get(date);
   }
 
-  public void add(String date, String fruit) {
+  public void add(LocalDate date, String fruit) {
     if (this.stock.containsKey(date)) {
         this.stock.get(date).add(fruit);
     }else {
--- src/test/java/org/example/InventoryTest.java
+++ src/test/java/org/example/InventoryTest.java
@@ -2,37 +2,41 @@ package org.example;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import org.junit.jupiter.api.Test;
 
 class InventoryTest {
 
   private Inventory inventory;
 
+  private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+  private final LocalDate today = LocalDate.parse("08/11/2024", formatter);
+
+  private final LocalDate yesterday = LocalDate.parse("07/11/2024", formatter);
+
   @Test
   void addProductToday() {
-    final String date = "08/11/2024";
-      this.inventory = new Inventory();
-      this.inventory.add(date, "Banana");
-    assertThat(this.inventory.project(date)).contains("Banana");
+    this.inventory = new Inventory();
+    this.inventory.add(today, "Banana");
+    assertThat(this.inventory.project(today)).contains("Banana");
   }
 
   @Test
   void addProductsToday() {
-    final String date = "08/11/2024";
     this.inventory = new Inventory();
-    this.inventory.add(date, "Banana");
-    this.inventory.add(date, "Apple");
-    assertThat(this.inventory.project(date)).containsExactly("Banana", "Apple");
+    this.inventory.add(today, "Banana");
+    this.inventory.add(today, "Apple");
+    assertThat(this.inventory.project(today)).containsExactly("Banana", "Apple");
   }
 
-
   @Test
-  void addProductYesterday(){
-    final String yesterday = "07/11/2024";
-    final String today = "08/11/2024";
-      this.inventory = new Inventory();
+  void addProductYesterday() {
+    this.inventory = new Inventory();
     this.inventory.add(today, "Banana");
-      this.inventory.add(yesterday, "Apple");
-    assertThat(this.inventory.project(today)).containsExactly("Banana","Apple");
+    this.inventory.add(yesterday, "Apple");
+    assertThat(this.inventory.project(today)).containsExactly("Banana", "Apple");
   }
 }
