da427db026ab10527018a601fe981bef2ae7d99a
SOrtega <163116793+SAOGoethe@users.noreply.github.com>
2024-11-08 15:17:49 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.11.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation("org.mockito:mockito-core:5.14.2")
+    testImplementation("com.approvaltests:approvaltests:24.9.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
